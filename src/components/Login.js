8edc1b8ea66a5b6c44c878fff74d1cc932ff0c0e
Thomas Lawrence <thomas66690801@gmail.com>
2019-01-25 10:56:42 +1100

    loginPageCommit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import '../styles/login.css'
 
 
 class Login extends Component {
   render() {
     return (
-      <div className="App">
-        This is the Login Page.
+      <div className="loginPage">
+        <h1>IMAGISTIC ADMIN</h1>
+          <div className="titleLine">
+          </div>
+        <form className="loginForm" action="">
+       
+      
+        <input type="text" placeholder='USERNAME'/>
+        <br/>
+        <br/>
+       
+        <input type="password" placeholder='PASSWORD'/>
+        <br/>
+        <button>Login</button>
+        
+        
+        </form>
+
+
+
+
       </div>
     );
   }
