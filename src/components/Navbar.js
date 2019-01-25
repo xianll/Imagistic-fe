eb0f2ee29e7999d29795c099131d5ce483024050
Yong Liang <yongl9460@gmail.com>
2019-01-25 13:03:59 +1100

    Added Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-
+import '../styles/Navbar.css'
 
 class Navbar extends Component {
   render() {
     return (
-      <div className="App">
-        This is the Navbar.
+      <div className="navbar">
+          <a href="/" className="logo">IMAGISTIC</a>
+        <div className="nav_links">
+          <a href="/gallery">GALLERY</a>
+          <a href="/about">ABOUT</a>
+          <a href="/contact">CONTACT</a>
+        </div>
       </div>
     );
   }
