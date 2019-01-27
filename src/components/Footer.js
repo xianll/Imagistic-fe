27a1ad1e7ef7d63f1f91bdb6c67ec77fa29f7073
Thomas Lawrence <thomas66690801@gmail.com>
2019-01-27 21:23:31 +1100

    improving aboutme page, with nav bar and link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div className="App">
-        This is the Footer.
+      <div className="footNote">
+        &#169; Copyright 2010 - 2020 David Sumner
       </div>
     );
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,14 @@ import React, { Component } from 'react';
 class Navbar extends Component {
   render() {
     return (
-      <div className="App">
-        This is the Navbar.
+      <div className="navBar">
+        <div className="logo">
+        IMAGISTIC
+        </div>
+        <a href="#">About</a>
+        <a href="#">Gallery</a>
+        
+
       </div>
     );
   }
