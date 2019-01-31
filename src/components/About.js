0940bc1f0a48ee00b67c88cf8dfb611755e7f6aa
Yong Liang <yongl9460@gmail.com>
2019-01-31 21:09:12 +1100

    fixed about and contact page

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ import Footer from './Footer';
 class About extends Component {
   render() {
     return (
-      <div class="pageContainer">
+      <div class="aboutPageContainer">
 
         <nav>
           <Navbar /> 
@@ -32,6 +32,7 @@ class About extends Component {
 My name is David and I am an amateur nature/wildlife and landscape photographer based in Berwick, Melbourne, Victoria. Please feel free to browse my website and the images in the Albums. Should you wish to commission, use or purchase an image then please contact me via the Contact Page.
 <br/>
 <br/>
+<br/>
 <strong>Please note that all images on this site are subject to copyright ©
 </strong>
 <br/>
@@ -49,7 +50,7 @@ David Sumner LAPS
 
         </div>
 
-        <footer>
+        <footer className="aboutFootNote">
           <Footer />
         </footer>
       </div>
